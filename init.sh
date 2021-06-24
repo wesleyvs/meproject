baee06745f7e9047da392d76fa3a551bf98b8085
wesleyvs <42287703+wesleyvs@users.noreply.github.com>
2021-06-24 04:26:30 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 psql -c 'CREATE DATABASE meprojectdatabase';
 psql -d meprojectdatabase -c 'CREATE SCHEMA meproject';
-psql -d meprojectdatabase -c "create table meproject.pessoa_fisica (id int primary key, pessoa_nome varchar(50) not null, pessoa_data_nascimento varchar(50) not null, pessoa_estado_civil varchar(60) not null, pessoa_dependentes varchar(100), check (pessoa_estado_civil in ('solteiro(a)', 'união estável', 'casado(a)', 'divorciado(a)', 'viuvo(a)')))"
+psql -d meprojectdatabase -c "create table pessoa_fisica (id int primary key, pessoa_nome varchar(50) not null, pessoa_data_nascimento varchar(50) not null, pessoa_estado_civil varchar(60) not null, pessoa_dependentes varchar(100), check (pessoa_estado_civil in ('solteiro(a)', 'união estável', 'casado(a)', 'divorciado(a)', 'viuvo(a)')))"
+psql -c 'CREATE ROLE postgres WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION';
 cd me-project/
 mvn install
 mvn spring-boot:run
