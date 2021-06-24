20632837c237daae54a7ba5c28b6dcd905b90a28
wesleyvs <42287703+wesleyvs@users.noreply.github.com>
2021-06-24 04:55:48 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 psql -c 'CREATE ROLE postgres WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION';
 psql -c "CREATE DATABASE meprojectdatabase" -c "CREATE SCHEMA meproject" -c "SET search_path TO meproject" -c "create table pessoa_fisica (id int primary key, pessoa_nome varchar(50) not null, pessoa_data_nascimento varchar(50) not null, pessoa_estado_civil varchar(60) not null, pessoa_dependentes varchar(100), check (pessoa_estado_civil in ('solteiro(a)', 'união estável', 'casado(a)', 'divorciado(a)', 'viuvo(a)')))" -U postgres
 cd me-project/
-sleeps 10
+echo 'O projeto será instalado e executado em 10 segundos...'
+sleep 10
 mvn install
 mvn spring-boot:run
