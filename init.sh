9eea68bd3bb8d07b9f2abeabc81c9aba791d6195
wesleyvs <42287703+wesleyvs@users.noreply.github.com>
2021-06-24 04:15:48 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 psql -c 'CREATE DATABASE meprojectdatabase';
 psql -d meprojectdatabase -c 'CREATE SCHEMA meproject';
 psql -d meprojectdatabase -c "create table meproject.pessoa_fisica (id int primary key, pessoa_nome varchar(50) not null, pessoa_data_nascimento varchar(50) not null, pessoa_estado_civil varchar(60) not null, pessoa_dependentes varchar(100), check (pessoa_estado_civil in ('solteiro(a)', 'união estável', 'casado(a)', 'divorciado(a)', 'viuvo(a)')))"
+cd me-project/
+mvn install
+mvn spring-boot:run
